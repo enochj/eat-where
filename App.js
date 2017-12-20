77127d03e94767f12ee42573845fc9f527010240
Enoch Jennison <enochjennison@gmail.com>
2017-12-20 16:33:03 -0600

    Base page added

--- App.js
+++ App.js
@@ -1,23 +1,86 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { 
+  Alert,
+  AppRegistry,
+  Button,
+  FlatList,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from 'react-native';
 
-export default class App extends React.Component {
+export default class LotsOfStyles extends Component {
+    _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior="padding">
+        <View style={styles.top}></View>
+        <FlatList
+          style={{flex: 1}}
+          data={[
+            {key: 'Tanyas Soup Kitchen'},
+            {key: 'Anchor'},
+            {key: 'Dempseys'},
+            {key: 'Picassos'},
+            {key: 'Little Saigon'},
+            {key: 'Uno Mas'},
+            {key: 'Flying Stove'},
+            {key: 'Caesars Palace'},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Add Place"
+            color="#841584"
+          />
+        </View>
+        <TextInput 
+          style={styles.where}
+          placeholder="Where will you eat today?"
+          onChangeText={(text) => this.setState({text})}
+        />
+      </KeyboardAvoidingView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    margin: 20
+  },
   container: {
+    flex:1,
+    flexDirection: 'column',
+    backgroundColor: 'cornsilk',
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
+  },
+  top: {
+    height: 100,
+  },
+  where: {
+    height: 100,
+    fontSize: 30,
+    backgroundColor: 'bisque',
+    textAlign: 'center'
+  },
+  item: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => LotsOfStyles);
